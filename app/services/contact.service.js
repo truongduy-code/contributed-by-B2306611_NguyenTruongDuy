1b1b70d2b3f0500cdca4abd921fd7d7127dcdc8a
truongduy-code <duynguyen.280905@gmail.com>
2026-02-28 03:08:35 +0700

    Cai dat cac handle truy xuat CSDL

--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -4,6 +4,28 @@ class ContactService {
     constructor(client) {
         this.Contact = client.db().collection("contacts");
     }
+    extractContactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            address: payload.address,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+        Object.keys(contact).forEach(
+            (key) => contact[key] === undefined && delete contact[key]
+        );
+        return contact;
+    }
+    async create(payload) {
+        const contact = this.extractContactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: { favorite: contact.favorite === true } },
+            { returnDocument: "after", upsert: true }
+        );
+        return result;
+    }
     async find(filter) {
         const cursor = await this.Contact.find(filter);
         return await cursor.toArray();
